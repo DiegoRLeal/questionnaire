dbd30c503146fd8f4f89aeb0deb6a680ba21b524
DiegoRLeal <leal.goedi@gmail.com>
2022-05-26 20:52:05 -0300

    Questionnaire

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :questionnaires
   devise_for :users
-  root to: 'pages#home'
+  root to: 'questionnaires#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
